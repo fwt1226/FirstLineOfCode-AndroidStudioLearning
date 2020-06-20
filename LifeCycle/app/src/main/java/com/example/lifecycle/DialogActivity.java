c36271b543eb14e3715ac3e580bb4c1c08cd969a
fwt1226 <24296347@qq.com>
2020-06-20 17:29:57 +0800

    nothing

---
+++ LifeCycle/app/src/main/java/com/example/lifecycle/DialogActivity.java
@@ -0,0 +1,16 @@
+package com.example.lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class DialogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+    }
+}
